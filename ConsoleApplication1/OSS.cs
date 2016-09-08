3c27bd93502b9678b3f241f48e99dcb221a43c0a
Hookist <bakalostas@gmail.com>
2016-09-08 21:49:02 +0300

    fill some billing func

---
+++ ConsoleApplication1/OSS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class account
+    {
+        // User $ count status
+        public IUser user;
+        public decimal money;
+    }
+}
---
+++ ConsoleApplication1/RSS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class RSS
+    {
+        //rules of relation
+        //Dictionary
+        //$-oper
+        //$-status
+        //$-User
+        //opper-status
+        // ...
+        private Dictionary<bool, string> account_state = new Dictionary<bool, string>();
+    }
+}
