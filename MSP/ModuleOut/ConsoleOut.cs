32ecea98ffc54a2eaaac3cbf4384ece5e8dfb075
ser910 <degtiar.sergey@gmail.com>
2016-09-23 20:22:46 +0300

    add 1

---
+++ MSP/ModuleOut/ConsoleOut.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using static System.Console;
+
+namespace ModuleOut
+{
+    public class ConsoleOut : IOut
+    {
+        private object result;
+        private IFormatter format;
+  
+
+        public ConsoleOut(object result)
+        {
+            this.result = result;
+        }
+
+        public object Format(object result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Out(object result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+        }
+
+        public string Precision
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string prompt
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Type type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Out(object result)
+        {
+            WriteLine()
+        }
+    }
+}
---
+++ MSP/ModuleOut/IOut.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleOut
+{
+    /// <summary>
+    /// interface for output result of calculations
+    /// </summary>
+    public interface IOut
+    {
+
+        string Format { get; }
+        /// <summary>
+        /// type of result 
+        /// </summary>
+        Type type { get; }
+        /// <summary>
+        /// string witch will be prompted before result
+        /// </summary>
+        string Prompt { get; }
+        string Value { get; }
+        /// <summary>
+        /// Precision of result
+        /// </summary>
+        string Precision();
+        void Out(object result);
+    }
+}
